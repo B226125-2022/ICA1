0c8f4d51f9f1ab20306507b0e626fdf9b8fb9bda
B226125-2022 <email_not_provided>
2022-10-15 13:39:06 +0100

    git ignore and script

--- Tcongo_genome/alignment_script.sh
+++ Tcongo_genome/alignment_script.sh
@@ -5,7 +5,11 @@ THREAD=64
 
 for READPAIR1 in ./*_1.fq
   do
-    bowtie2 --no-unal -p $THREAD -x $INDEXGENOME -1 $READPAIR1 -2 ${READPAIR1/_1/_2} -S testoutput.sam
+    echo $READPAIR1
+    echo ${READPAIR1/_1/_2}
+    bowtie2 --no-unal -p $THREAD -x $INDEXGENOME -1 $READPAIR1 -2 ${READPAIR1/_1/_2} -S samfile.sam 
+    for samfile.sam in ./
+      samtools view -S -b samfile.sam > bamfile.bam
 
   done 
     
